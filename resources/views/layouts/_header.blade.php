d6ef0534156d1e3941e7e33d9e613ce6a448f6bd
Fjun <921516@qq.com>
2019-09-21 11:24:44 +0800

    修改正常跳转

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -10,9 +10,30 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto"></ul>
+
             <ul class="navbar-nav navbar-right">
+                @guest
                 <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">登陆</a></li>
                 <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">注册</a></li>
+                @else
+                <li class="navbar-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" alt="" class="img-responsive img-circle" width="30px;" height="30px">
+                        {{ Auth::user()->name }}
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a href="" class="dropdown-item">个人中心</a>
+                        <a href="" class="dropdown-item">编辑资料</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" id="logout" href="">
+                            <form action="{{ route('logout') }}" method="POST">
+                                {{ csrf_field() }}
+                                <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                            </form>
+                        </a>
+                    </div>
+                </li>
+                @endguest
             </ul>
         </div>
     </div>
