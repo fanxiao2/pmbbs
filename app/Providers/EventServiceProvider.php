bf02201546c2ca7e02c9434eada30004734ffa66
Fjun <921516@qq.com>
2019-09-23 10:43:45 +0800

    邮箱认证通过提醒

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -15,8 +15,11 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
+        
+
+            \Illuminate\Auth\Events\Verified::class => [
+                \App\Listeners\EmailVerified::class,
+            
         ],
     ];
 
