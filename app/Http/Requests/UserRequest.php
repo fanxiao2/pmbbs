d995722ca202b218745f3c1363bf633f7c3de64e
Fjun <921516@qq.com>
2019-09-24 09:37:35 +0800

    头像上传规格

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -28,6 +28,7 @@ class UserRequest extends FormRequest
             'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
             'email' => 'required|email',
             'introduction' => 'max:80',
+            'avatar' => 'mimes:jpeg,bmp,png,gif,|dimensions:min_width=208,min_height=208',
         ];
     }
 
@@ -35,6 +36,8 @@ class UserRequest extends FormRequest
     public function messages()
     {
         return[
+            'avatar.mimes' => '头像必须是:jpeg, bmp, png, gif格式的图片',
+            'avatar.dimensions' => '图片的清晰度不够，宽和高需要208px以上',
             'name.unique' => '用户名已被占用，请重新输入',
             'name.regex'  => '用户名只支持英文、数字、下划线',
             'name.between' => '用户名必须介于3-25字符之间',
