80aeb1d79b9cb2acffc422edce36a96f76edb5ce
Fjun <921516@qq.com>
2019-09-24 08:06:36 +0800

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
@@ -21,8 +22,17 @@ class UsersController extends Controller
     }
 
     //处理编辑表单，更新内容
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+        $data = $request->all();
+
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
         $user->update($request->all());
         return redirect()->route('users.show', $user->id)->with('success', '个人资料修改成功');
     }
