66014bc57aa6d45d6a40487da0928cf066bb68c9
Fjun <921516@qq.com>
2019-10-08 22:25:35 +0800

    优化话题内容页面

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'PM-bbs')- 产品经理社区</title>
+    <meta name="description" content="@yield('description', 'pmku 产品经理社区')">
 
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     @yield('styles')
