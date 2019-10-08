3cba1e42fc23996451dc06dee947bb4af30f20c3
fanxiao2 <921516@qq.com>
2019-10-08 10:58:49 +0800

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,5 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 // 资源路由
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
