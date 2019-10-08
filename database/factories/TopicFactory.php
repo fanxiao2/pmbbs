62218f63b357d3a48a0ae72162f01bd505706f31
fanxiao2 <921516@qq.com>
2019-10-08 12:13:56 +0800

    填充数据

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,7 +3,18 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+    
+    // 'name' => $faker->name,
+    $sentence = $faker->sentence();
+    //随机取一个月时间
+    $updated_at = $faker->dateTimeThisMonth();
+    // 传参为生成最大时间（更新时间比创建时间永远都晚）
+    $created_at = $faker->dateTimeThisMonth($updated_at);
     return [
-        // 'name' => $faker->name,
+        'title' => $sentence,
+        'body' => $faker->text(),
+        'excerpt' => $sentence,
+        'created_at' => $created_at,
+        'updated_at' => $updated_at,
     ];
 });
